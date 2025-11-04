b87c1b733ffaa62f0a9322e5134ea00dc649d643
Jithin2k <jithinjmadhav@gmail.com>
2025-11-04 15:03:38 +0530

    Optional Props in TS

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,21 +1,21 @@
-
-import "./App.css"
-import Greet from "./Components/Greet"
-import Heading from "./Components/Heading"
-import Oscar from "./Components/Oscar"
-import Status from "./Components/Status"
+import "./App.css";
+import Greet from "./Components/Greet";
+import Heading from "./Components/Heading";
+import Oscar from "./Components/Oscar";
+import Status from "./Components/Status";
 
 const App = () => {
   return (
     <div>
-      <Greet name = "Jithin" num={11}/>
-      <Status status="success"/>
+      <Greet name="Jithin" num={11}  />
+      <Status status="success" />
       <Heading>PlaceHolder Text</Heading>
       <Oscar>
         <Heading>Oscar goes to Mohanlal</Heading>
       </Oscar>
+      <Greet name="Viswas" num={89} isLoggedIn={true}/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- react-typescript/src/Components/Greet.tsx
+++ react-typescript/src/Components/Greet.tsx
@@ -1,12 +1,16 @@
 type GreetProps = {
     name : string,
-    num : number
+    num : number,
+    isLoggedIn? : boolean
 }
 
-const Greet = ({name,num} : GreetProps) => {
+const Greet = ({name,num,isLoggedIn} : GreetProps) => {
   return (
     <div>
-      <h1>Hello,Welcome {name}!You have {num} messages</h1>
+      {
+        isLoggedIn ?  <h1>Hello,Welcome {name}!You have {num} messages.</h1> : <h2>Welcome All</h2>
+      }
+     
     </div>
   )
 }
