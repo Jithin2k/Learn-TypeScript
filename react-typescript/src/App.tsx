a8d7f030727a9eba834d9e66a6bad29a70b3fb67
Jithin2k <jithinjmadhav@gmail.com>
2025-11-04 14:41:47 +0530

    Union operator in props

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,11 +1,13 @@
 
 import "./App.css"
 import Greet from "./Components/Greet"
+import Status from "./Components/Status"
 
 const App = () => {
   return (
     <div>
       <Greet name = "Jithin" num={11}/>
+      <Status status="success"/>
     </div>
   )
 }
