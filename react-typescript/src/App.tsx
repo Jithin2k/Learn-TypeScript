06ae50bd3beec2b5cc418ec0bfe8475f8ce45859
Jithin2k <jithinjmadhav@gmail.com>
2025-11-04 15:24:14 +0530

    Event type -void in TS

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Button from "./Components/Button";
 import Greet from "./Components/Greet";
 import Heading from "./Components/Heading";
 import Oscar from "./Components/Oscar";
@@ -7,13 +8,18 @@ import Status from "./Components/Status";
 const App = () => {
   return (
     <div>
-      <Greet name="Jithin" num={11}  />
+      <Greet name="Jithin" num={11} />
       <Status status="success" />
       <Heading>PlaceHolder Text</Heading>
       <Oscar>
         <Heading>Oscar goes to Mohanlal</Heading>
       </Oscar>
-      <Greet name="Viswas" num={89} isLoggedIn={true}/>
+      <Greet name="Viswas" num={89} isLoggedIn={true} />
+      <Button
+        handleClick={() => {
+          console.log("clicked");
+        }}
+      />
     </div>
   );
 };
---
+++ react-typescript/src/Components/Button.tsx
@@ -0,0 +1,11 @@
+type ButtonProps = {
+    handleClick : () => void
+}
+
+const Button = (props : ButtonProps) => {
+  return (
+   <button onClick={props.handleClick}>Click</button>
+  )
+}
+
+export default Button
